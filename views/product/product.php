adc8f77d09b56763eff59b7397252374d711457b
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-28 00:21:25 +0200

    Ajouter la vue produit avec récupération des données nécessaires

---
+++ views/product/product.php
@@ -0,0 +1,8 @@
+<?php
+
+// Récupération et sécurisation des données transmises par le contrôleur
+$product = $data['productInfo'] ?? [];
+$gallery = $data['gallery'] ?? [];
+$csrfToken = $data['csrf_token'] ?? '';
+$userId = Model::sessionGet('userId');
+
