d7b03f230bbe36b608ffbd42da8830e7a3015f95
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-27 23:50:24 +0200

    la méthode findProductImage pour gérer l'affichage des images de produits

--- models/ModelProduct.php
+++ models/ModelProduct.php
@@ -8,5 +8,20 @@ class ModelProduct extends Model
     }
 
 
+    public function findProductImage($id, $size = 350) 
+    {
+        $basePath = 'public/images/products/' . (int)$id . '/product_' . (int)$size;
+        $extensions = ['jpg', 'webp', 'png', 'jpeg'];
+        
+        foreach ($extensions as $ext) {
+            if (file_exists($basePath . '.' . $ext)) {
+                return URL . $basePath . '.' . $ext . '?v=' . time();
+            }
+        }
+        return 'https://placehold.co/' . (int)$size . 'x' . (int)$size . '/f8f9fa/adb5bd?text=Image';
+    }
+
+   
+    
 }
 ?>
