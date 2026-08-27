d1643352e8eda492ac274e92bc516e980dcf0863
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-28 00:38:47 +0200

    Ajout de la section des exclusivités avec en-tête

--- views/product/exclusives.php
+++ views/product/exclusives.php
@@ -2,3 +2,10 @@
 
 $exclusives = $data['exclusives'] ?? [];
 ?>
+
+<div class="exclusive-section">
+    <div class="exclusive-header">
+        <h3><i class="fa-solid fa-star" aria-hidden="true"></i> Exclusivités Boutique</h3>
+    </div>
+    
+   
