f2b0ea5947bfc9fd225405da686a9231f3412dfa
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-27 10:20:45 +0200

    Ajout de la méthode __construct() pour récupérer et découper l'URL

--- core/app.php
+++ core/app.php
@@ -6,5 +6,14 @@ class App
     protected $method = 'index';
     protected $params = [];
 
-    
+    public function __construct()
+    {
+        // 1. Récupération et découpage de l'URL
+        if (isset($_GET['url'])) {
+            $url = $this->parseUrl($_GET['url']);
+
+           
+        }
+
+       
 }
