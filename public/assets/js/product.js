9fe6b76cc8027136050c8e0dbe4db38ff5d1e05f
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-28 00:53:35 +0200

    Empêcher l'exécution multiple de scripts lors du rechargement

---
+++ public/assets/js/product.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Empêche les exécutions multiples du script lors de rechargements partiels
+    if (window.productScriptEventsBound) return;
+    window.productScriptEventsBound = true;
+
+    
+
+});
