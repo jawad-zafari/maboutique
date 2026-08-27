1dfeca54b443a4b1489d976ad97d0ddc8515af6f
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-27 10:22:31 +0200

    la validation des caractères spéciaux

--- core/app.php
+++ core/app.php
@@ -12,8 +12,25 @@ class App
         if (isset($_GET['url'])) {
             $url = $this->parseUrl($_GET['url']);
 
-           
+            if (!empty($url[0])) {
+                $this->controller = ucfirst($url[0]);
+                unset($url[0]);
+            }
+
+            if (isset($url[1])) {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+
+            // Réindexer le tableau des paramètres restants
+            $this->params = array_values($url);
         }
 
-       
+        // Bloquer les caractères spéciaux 
+        if (!preg_match('/^[a-zA-Z0-9_]+$/', $this->controller) || !preg_match('/^[a-zA-Z0-9_]+$/', $this->method)) {
+            die("Erreur de sécurité : Caractères non autorisés détectés dans l'URL.");
+        }
+
+        
+    }
 }
