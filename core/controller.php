a54f29527188735e4e0c717225c1723d2f824bd3
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-27 10:33:13 +0200

    Ajout de la méthode __construct()

--- core/controller.php
+++ core/controller.php
@@ -5,5 +5,15 @@ class Controller
 {
     protected $model;
 
-    
+    public function __construct()
+    {
+        // Détecter automatiquement le nom du contrôleur enfant
+        $controllerName = get_class($this);
+
+     
+    }
+
+   
+   
+}
 ?>
