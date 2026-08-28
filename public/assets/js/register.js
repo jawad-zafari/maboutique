e6db5a1ad7fae22e9af57ce185a5ed846124aa19
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-28 09:46:28 +0200

    ajout du script d'enregistrement avec gestion des erreurs

---
+++ public/assets/js/register.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const formRegister = document.getElementById('formRegister');
+    const errorContainer = document.getElementById('jsRegisterErrorMessage');
+    
+   
+});
