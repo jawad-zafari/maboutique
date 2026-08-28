b91126cd9c094ab87710c194db128e54c41d8341
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-28 09:16:02 +0200

    méthode insertUser pour l'insertion des utilisateurs avec nettoyage de l'e-mail

--- models/ModelRegister.php
+++ models/ModelRegister.php
@@ -8,6 +8,13 @@ class ModelRegister extends Model
     }
     
 
-   
+    public function insertUser(array $data): bool 
+    {
+        // Nettoyage de l'e-mail
+        $email = filter_var($data['email'] ?? '', FILTER_SANITIZE_EMAIL);
+        $password = $data['password'] ?? '';
+        
+       
+    }
 }
 ?>
