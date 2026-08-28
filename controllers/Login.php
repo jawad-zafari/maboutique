c04def19a5221a165a3cbf1425bd64de4475e28d
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-28 11:45:13 +0200

    méthode d'authentification pour traiter la soumission du formulaire

--- controllers/Login.php
+++ controllers/Login.php
@@ -20,6 +20,16 @@ class Login extends Controller
         $this->view('login/login', $data);
     }
 
-   
+    // Traite la soumission du formulaire et authentifie l'utilisateur
+    public function authenticate(): void
+    {
+        // SÉCURITÉ : Bloquer les requêtes qui ne sont pas de type POST
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            header('HTTP/1.1 405 Method Not Allowed');
+            exit;
+        }
+
+        
+    }
 }
 ?>
