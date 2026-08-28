b75b7d938a5da1c84c6f18b00a93c0f660187bee
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-28 12:04:50 +0200

    gestion de l'événement

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -4,4 +4,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const formLogin = document.getElementById('formLogin');
     const errorContainer = document.getElementById('jsLoginErrorMessage');
     
-   
+    if (formLogin && errorContainer) {
+        formLogin.addEventListener('submit', (event) => {
+            
+            const emailInput = document.getElementById('email');
+            const passwordInput = document.getElementById('password');
+            
+            let isValid = true;
+            let errorMessages = [];
+
+            
