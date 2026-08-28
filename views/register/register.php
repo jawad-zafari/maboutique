fa64446443fba2c587cfec2b96f435118fb3aec9
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-28 09:34:14 +0200

    structure de la page d'inscription avec les informations et avantages

--- views/register/register.php
+++ views/register/register.php
@@ -4,3 +4,29 @@
 $csrfToken = $data['csrf_token'] ?? '';
 ?>
 
+<div class="register-container">
+    
+    <div class="register-box">
+        
+        <div class="register-info">
+            <div class="info-icon">
+                <i class="fa-solid fa-user-plus" aria-hidden="true"></i>
+            </div>
+            <h2>Créer un compte client</h2>
+            <ul class="benefits-list">
+                <li>
+                    <i class="fa-solid fa-bolt" aria-hidden="true"></i> 
+                    <span>Achetez plus rapidement et plus simplement</span>
+                </li>
+                <li>
+                    <i class="fa-solid fa-list-check" aria-hidden="true"></i> 
+                    <span>Gérez facilement votre historique d'achats</span>
+                </li>
+                <li>
+                    <i class="fa-solid fa-heart" aria-hidden="true"></i> 
+                    <span>Créez vos listes d'envies et favoris</span>
+                </li>
+            </ul>
+        </div>
+
+        
