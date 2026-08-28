8cf3346fe3309031aea2e934846dd3ebbaa2b047
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-28 09:36:40 +0200

    formulaire d'inscription avec validation des champs et options d'abonnement

--- views/register/register.php
+++ views/register/register.php
@@ -51,4 +51,61 @@ $csrfToken = $data['csrf_token'] ?? '';
                 </div>
             <?php endif; ?>
 
-        
+            <form action="<?= URL ?>Register/save" method="post" id="formRegister" class="modern-form" autocomplete="off">
+                
+                <input type="hidden" name="csrf_token" value="<?= htmlspecialchars($csrfToken, ENT_QUOTES, 'UTF-8') ?>">
+
+                <div class="form-group">
+                    <label for="lastName">Nom complet * :</label>
+                    <input type="text" id="lastName" name="last_name" class="form-control" placeholder="Jean Dupont" autocomplete="name" required aria-required="true">
+                </div>
+
+                <div class="form-group">
+                    <label for="mobile">Numéro de mobile * :</label>
+                    <input type="tel" id="mobile" name="mobile" class="form-control" dir="ltr" placeholder="0612345678" autocomplete="tel" required aria-required="true">
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Adresse E-mail * :</label>
+                    <input type="email" id="email" name="email" class="form-control" dir="ltr" placeholder="exemple@email.com" autocomplete="email" required aria-required="true">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Mot de passe (min. 6 caractères) * :</label>
+                    <input type="password" id="password" name="password" class="form-control" dir="ltr" placeholder="••••••••" autocomplete="new-password" required aria-required="true">
+                </div>
+
+                <div class="form-group">
+                    <label for="passwordConfirm">Confirmer le mot de passe * :</label>
+                    <input type="password" id="passwordConfirm" name="password_confirm" class="form-control" dir="ltr" placeholder="••••••••" autocomplete="new-password" required aria-required="true">
+                </div>
+
+                <div class="checkbox-group">
+                    <input type="checkbox" id="rules" name="rules" value="1" required aria-required="true">
+                    <label for="rules">
+                        J'ai lu et j'accepte les <a href="<?= URL ?>Page/conditions" class="terms-link">conditions générales</a>
+                    </label>
+                </div>
+
+                <div class="checkbox-group">
+                    <input type="checkbox" id="newsletter" name="newsletter" value="1">
+                    <label for="newsletter">S'abonner à la newsletter pour recevoir nos offres</label>
+                </div>
+
+                <div class="form-actions">
+                    <button type="submit" class="btn-action" aria-label="Valider l'inscription">
+                        <span>S'inscrire</span>
+                        <i class="fa-solid fa-arrow-right" aria-hidden="true"></i>
+                    </button>
+                </div>
+
+                <div class="login-redirect">
+                    <span>Déjà inscrit ?</span> 
+                    <a href="<?= URL ?>Login/index" class="login-link">Se connecter</a>
+                </div>
+
+            </form>
+        </div>
+
+    </div>
+</div>
