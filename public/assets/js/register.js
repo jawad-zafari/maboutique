a1314f9e9c306055da155362d3ce3a1f50c71afb
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-28 09:48:21 +0200

    gestion de l'événement de soumission du formulaire d'enregistrement

--- public/assets/js/register.js
+++ public/assets/js/register.js
@@ -3,5 +3,20 @@ document.addEventListener("DOMContentLoaded", () => {
     const formRegister = document.getElementById('formRegister');
     const errorContainer = document.getElementById('jsRegisterErrorMessage');
     
-   
+    if (formRegister && errorContainer) {
+        formRegister.addEventListener('submit', (event) => {
+            
+            const lastNameInput = document.getElementById('lastName');
+            const mobileInput = document.getElementById('mobile');
+            const emailInput = document.getElementById('email');
+            const passwordInput = document.getElementById('password');
+            const passwordConfirmInput = document.getElementById('passwordConfirm');
+            const rulesCheckbox = document.getElementById('rules');
+            
+            let isValid = true;
+            let errors = []; 
+
+            
+    }
+
 });
