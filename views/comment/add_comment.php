adc8a6811fd944b3edc29815fe2c929f7226c937
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-31 01:02:07 +0200

    ajout du formulaire de commentaire avec protection CSRF

--- views/comment/add_comment.php
+++ views/comment/add_comment.php
@@ -8,3 +8,10 @@ $csrfToken     = $data['csrf_token'] ?? '';
 $productId     = (int)($productInfo['id'] ?? 0);
 ?>
 
+<div class="comment-container">
+    <form method="post" action="<?= URL ?>AddComment/saveComment/<?= $productId ?>" class="comment-form" id="formComment">
+        
+        <!-- SÉCURITÉ : Jeton CSRF -->
+        <input type="hidden" name="csrf_token" value="<?= $this->e($csrfToken) ?>">
+
+       
