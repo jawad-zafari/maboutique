aaac7df43dd12d53949cd50b90269328d52ff29a
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-30 22:58:08 +0200

    les filtres et les données nécessaires

---
+++ views/search/search.php
@@ -0,0 +1,9 @@
+<?php
+
+$sideFilters = $data['attrRight'] ?? [];
+$colors      = $data['colors'] ?? [];
+$topFilters  = $data['attr'] ?? [];
+$categoryId  = (int)($data['categoryId'] ?? 0);
+$keyword     = $data['keyword'] ?? '';
+$csrfToken   = $data['csrf_token'] ?? '';
+?>
