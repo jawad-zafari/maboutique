c31774ac679ac5614eb037793c38e207fbf5ba0d
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-30 22:49:06 +0200

    méthode index pour la recherche avec assainissement des entrées et protection CSRF

--- controllers/Search.php
+++ controllers/Search.php
@@ -9,6 +9,30 @@ class Search extends Controller
         Model::sessionInit();
     }
 
+    // Page principale de recherche
+    public function index(string $categoryId = '0'): void
+    {
+        // Cast en entier pour éviter les injections via l'URL
+        $categoryIdInt = (int)$categoryId;
+        
+        $attributes = $this->model->getAttr($categoryIdInt);
+        $attributesRight = $this->model->getAttrRight($categoryIdInt);
+        $colors = $this->model->getColors();
+        
+        // Assainissement strict du mot-clé (Input Sanitization)
+        $keyword = isset($_POST['keyword']) ? trim(strip_tags($_POST['keyword'])) : '';
+        
+        $data = [
+            'attr'       => $attributes, 
+            'attrRight'  => $attributesRight, 
+            'colors'     => $colors,
+            'categoryId' => $categoryIdInt,
+            'keyword'    => $keyword,
+            // SÉCURITÉ : Jeton CSRF pour protéger les requêtes AJAX
+            'csrf_token' => $this->generateCsrfToken() 
+        ];
+        
+        $this->view('search/search', $data);
+    }
+
    
-}
-?>
