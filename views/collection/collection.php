5289100ec87de80d8b16fbdea46f72e27ab0c986
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-31 00:30:21 +0200

    navigation par fil d'Ariane

--- views/collection/collection.php
+++ views/collection/collection.php
@@ -25,4 +25,17 @@ elseif ($type === 'category') { $pageTitle = $data['categoryTitle'] ? $data['cat
 <!--Stockage du jeton CSRF pour les requêtes AJAX (ex: Ajout au panier) -->
 <div id="collectionMainWrapper" class="collection-container collection-page-wrapper" data-csrf="<?= $this->e($data['csrf_token'] ?? '') ?>">
     
-   
+    <nav class="breadcrumb-navigation" aria-label="Fil d'Ariane">
+        <button type="button" class="btn-go-back js-back-button" aria-label="Retour à la page précédente">
+            <i class="fa-solid fa-arrow-left" aria-hidden="true"></i> Retour
+        </button>
+        <span class="divider-icon" aria-hidden="true">|</span>
+        <a href="<?= URL ?>Index/index" class="link-home">
+            <i class="fa-solid fa-house" aria-hidden="true"></i> Accueil
+        </a>
+        <i class="fa-solid fa-angle-right divider-icon" aria-hidden="true"></i>
+        <!-- SÉCURITÉ (XSS) : Affichage sécurisé du titre -->
+        <span class="current-page-title" aria-current="page"><?= $this->e($pageTitle) ?></span>
+    </nav>
+
+    
