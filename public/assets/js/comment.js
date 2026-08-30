8b2758f533e20ab0ce7f88d17ce9d489a01896b0
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-31 01:09:58 +0200

    mise à jour du badge d'affichage

--- public/assets/js/comment.js
+++ public/assets/js/comment.js
@@ -8,7 +8,25 @@ document.addEventListener("DOMContentLoaded", () => {
         // Sélectionner le badge d'affichage correspondant à ce curseur
         const badge = input.nextElementSibling;
 
-       
+        // Écouter l'événement 'input' (mise à jour en temps réel lors du glissement)
+        input.addEventListener('input', function() {
+            // Utilisation de textContent (Anti-XSS)
+            badge.textContent = this.value;
+            
+            // Mise à jour pour les lecteurs d'écran
+            this.setAttribute('aria-valuenow', this.value);
+            
+            // Changement de couleur selon la note attribuée
+            if(this.value >= 4) {
+                badge.style.backgroundColor = '#36be2b';
+            } else if (this.value == 3) {
+                badge.style.backgroundColor = '#f39c12'; 
+            } else {
+                badge.style.backgroundColor = '#e74c3c'; 
+            }
+        });
+        
+        
     });
 
 });
