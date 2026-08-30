04e4bbf3f61b94e3bf8814d953c41b785f09c41e
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-31 01:09:11 +0200

    gérer les inputs de type range et leur badge d'affichage

--- public/assets/js/comment.js
+++ public/assets/js/comment.js
@@ -4,6 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
     // Sélectionner tous les inputs de type range
     const rangeInputs = document.querySelectorAll('.native-range');
 
-   
+    rangeInputs.forEach(input => {
+        // Sélectionner le badge d'affichage correspondant à ce curseur
+        const badge = input.nextElementSibling;
+
+       
+    });
 
 });
