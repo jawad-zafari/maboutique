3b43108eeb291233ce44d3f86f51258f1ccf11a0
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-30 22:54:34 +0200

    récupérer les attributs secondaires de filtre

--- models/ModelSearch.php
+++ models/ModelSearch.php
@@ -20,6 +20,19 @@ class ModelSearch extends Model
         return $result;
     }
 
+    // Récupère les attributs secondaires de filtre
+    public function getAttrRight(int $categoryId): array
+    {
+        $sql = "SELECT * FROM attributes WHERE category_id = ? AND is_right_filter = 1";
+        $result = $this->doSelect($sql, [$categoryId]);
+
+        foreach ($result as $key => $row) {
+            $sqlValues = "SELECT * FROM attribute_values WHERE attribute_id = ?";
+            $result[$key]['values'] = $this->doSelect($sqlValues, [(int)$row['id']]);
+        }
+        return $result;
+    }
+
    
 }
 ?>
