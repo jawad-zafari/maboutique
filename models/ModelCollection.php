737572939346e39cb8afc5bb12d178f34739a847
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-31 00:00:04 +0200

    récupérer les produits par catégorie

--- models/ModelCollection.php
+++ models/ModelCollection.php
@@ -8,6 +8,28 @@ class ModelCollection extends Model
     }
 
     
-  
+    public function getCollectionProducts(string $type, int $limit, int $offset, int $categoryId, array $filters): array
+    {
+        $products = [];
+        $total = 0;
+        $categoryTitle = '';
+
+        $whereClauses = [];
+        $params = [];
+
+        // Condition selon le type de collection
+        if ($type === 'category') {
+            // Requêtes préparées pour éviter les injections SQL
+            $whereClauses[] = "(category_id = ? OR secondary_category_id = ?)";
+            $params[] = $categoryId;
+            $params[] = $categoryId;
+
+            $sqlCat = "SELECT title FROM categories WHERE id = ?";
+            $catRes = $this->doSelect($sqlCat, [$categoryId], 'fetch');
+            if (!empty($catRes)) { 
+                $categoryTitle = $catRes['title']; 
+            }
+            
+        } 
 }
 ?>
