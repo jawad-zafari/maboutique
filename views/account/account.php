9a4861c5f3d67c1dbde5b0c3db56bfe110007492
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-30 18:15:36 +0200

    section "Mes favoris" et des boutons pour déconnexion et suppression de compte

--- views/account/account.php
+++ views/account/account.php
@@ -22,4 +22,22 @@ $latestOrder      = $latestOrder ?? ($ordersList[0] ?? null);
             <span class="user-since">Client(e) de la boutique</span>
         </div>
 
-       
+        <ul class="account-nav-list">
+            <li class="nav-item active" data-target="tabDashboard"><i class="fa-solid fa-chart-pie" aria-hidden="true"></i> Tableau de bord</li>
+            <li class="nav-item" data-target="tabOrders"><i class="fa-solid fa-box-open" aria-hidden="true"></i> Mes commandes</li>
+            
+            <li class="nav-item">
+                <a href="<?= URL ?>Account/favorites" class="account-sidebar-link">
+                    <i class="fa-solid fa-heart" aria-hidden="true"></i> Mes favoris
+                </a>
+            </li>
+            
+            <li class="nav-item" data-target="tabVouchers"><i class="fa-solid fa-ticket" aria-hidden="true"></i> Mes réductions</li>
+            <li class="nav-item" data-target="tabInfo"><i class="fa-solid fa-user-pen" aria-hidden="true"></i> Mes informations</li>
+            <li class="nav-separator"></li>
+            <li><button type="button" class="nav-link-danger" id="btnOpenLogoutModal"><i class="fa-solid fa-arrow-right-from-bracket" aria-hidden="true"></i> Déconnexion</button></li>
+            <li><button type="button" class="nav-link-danger delete-account-btn" id="btnOpenDeleteModal"><i class="fa-solid fa-user-xmark" aria-hidden="true"></i> Supprimer le compte</button></li>
+        </ul>
+    </aside>
+
+    
