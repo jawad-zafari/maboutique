771ffd331217c2cc104a78f3a6b6a5175cdb8b0a
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-30 17:09:11 +0200

    récupérer les informations d'un utilisateur

--- models/ModelAccount.php
+++ models/ModelAccount.php
@@ -7,6 +7,14 @@ class ModelAccount extends Model
         parent::__construct();
     }
 
-    
+    // Récupère les informations d'un utilisateur
+    public function getUserInfo(int $userId): array
+    {
+        $sql = "SELECT * FROM users WHERE id = ?";
+        $result = $this->doSelect($sql, [$userId]);
+        return $result[0] ?? [];
+    }
+
+   
 }
 ?>
