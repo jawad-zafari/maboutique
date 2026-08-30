07b149a5f765c736f7d251149e01a93f303fb41b
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-31 00:54:54 +0200

    récupérer les informations d'un produit

--- models/ModelAddComment.php
+++ models/ModelAddComment.php
@@ -6,6 +6,15 @@ class ModelAddComment extends Model
         parent::__construct();
     }
 
+    // Récupère les informations du produit
+    public function productInfo(int $productId): array
+    {
+        $sql = "SELECT * FROM products WHERE id = ?";
+        $result = $this->doSelect($sql, [$productId], 'fetch');
+        
+        return $result ?: [];
+    }
+
    
 }
 ?>
