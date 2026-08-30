d9819a08739f10cc63254f67fdf97778d043d8a8
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-31 01:08:37 +0200

    gestion des inputs de type range

---
+++ public/assets/js/comment.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    // Sélectionner tous les inputs de type range
+    const rangeInputs = document.querySelectorAll('.native-range');
+
+   
+
+});
