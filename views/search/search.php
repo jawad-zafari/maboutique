d2dd29e9271e57fd6304b0af86ec0b9e5e6c8de3
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-30 22:59:16 +0200

    formulaire de recherche et de filtrage

--- views/search/search.php
+++ views/search/search.php
@@ -7,3 +7,13 @@ $categoryId  = (int)($data['categoryId'] ?? 0);
 $keyword     = $data['keyword'] ?? '';
 $csrfToken   = $data['csrf_token'] ?? '';
 ?>
+<div class="search-container">
+    
+    <form id="searchForm" action="<?= URL ?>Search/doSearch" method="post" aria-label="Formulaire de recherche et de filtrage">
+        
+        <input type="hidden" name="categoryId" value="<?= $categoryId ?>">
+        <input type="hidden" name="keyword" value="<?= $this->e($keyword) ?>">
+        
+        <input type="hidden" id="globalCsrfToken" name="csrf_token" value="<?= $this->e($csrfToken) ?>">
+        
+        
