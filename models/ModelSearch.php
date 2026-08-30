7bf173024b5f1caae610dfaedec28ab332d8ea4b
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-30 22:53:42 +0200

    récupérer les attributs de filtre par catégorie

--- models/ModelSearch.php
+++ models/ModelSearch.php
@@ -7,6 +7,19 @@ class ModelSearch extends Model
         parent::__construct();
     }
 
-    
+    // Récupère les attributs de filtre pour la catégorie donnée
+    public function getAttr(int $categoryId): array
+    {
+        $sql = "SELECT * FROM attributes WHERE category_id = ? AND is_filter = 1";
+        $result = $this->doSelect($sql, [$categoryId]);
+        
+        foreach ($result as $key => $row) {
+            $sqlValues = "SELECT * FROM attribute_values WHERE attribute_id = ?";
+            $result[$key]['values'] = $this->doSelect($sqlValues, [(int)$row['id']]);
+        }
+        return $result;
+    }
+
+   
 }
 ?>
