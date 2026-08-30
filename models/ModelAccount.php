54db93cef01f2cfbe6d7515cf3ae05614b7496b9
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-30 17:10:01 +0200

    mettre à jour les informations de l'utilisateur

--- models/ModelAccount.php
+++ models/ModelAccount.php
@@ -15,6 +15,26 @@ class ModelAccount extends Model
         return $result[0] ?? [];
     }
 
-   
+    // Met à jour le profil avec les données nettoyées par le contrôleur
+    public function updateProfile(array $cleanData, int $userId): void
+    {
+        $sql = "UPDATE users SET username = ?, email = ?, last_name = ?, mobile = ?, phone = ?, address = ?, city = ?, postal_code = ?, gender = ?, newsletter = ? WHERE id = ?";
+        
+        $this->doQuery($sql, [
+            $cleanData['username'], 
+            $cleanData['email'], 
+            $cleanData['last_name'], 
+            $cleanData['mobile'], 
+            $cleanData['phone'], 
+            $cleanData['address'], 
+            $cleanData['city'], 
+            $cleanData['postal_code'], 
+            $cleanData['gender'], 
+            $cleanData['newsletter'], 
+            $userId
+        ]);
+    }
+
+    
 }
 ?>
