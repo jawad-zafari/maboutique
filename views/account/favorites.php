89cf68cc1c5f949905acfef7726a04d5ce0e1782
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-30 18:21:47 +0200

    page "Mes Favoris" avec navigation par fil d'Ariane

---
+++ views/account/favorites.php
@@ -0,0 +1,22 @@
+<div class="collection-container mt-30">
+    
+    <div class="breadcrumb-navigation">
+        <button type="button" class="btn-go-back js-back-button" aria-label="Retourner à la page précédente">
+            <i class="fa-solid fa-arrow-left" aria-hidden="true"></i> Retour
+        </button>
+        <span class="divider-icon">|</span>
+        
+        <a href="<?= URL ?>Index/index" class="link-home">
+            <i class="fa-solid fa-house" aria-hidden="true"></i> Accueil
+        </a>
+        <i class="fa-solid fa-angle-right divider-icon" aria-hidden="true"></i>
+        
+        <a href="<?= URL ?>Account/index" class="link-home">
+            <i class="fa-solid fa-user" aria-hidden="true"></i> Mon Compte
+        </a>
+        <i class="fa-solid fa-angle-right divider-icon" aria-hidden="true"></i>
+        
+        <span class="current-page-title">Mes Favoris</span>
+    </div>
+
+    
