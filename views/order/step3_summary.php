4419e57d71b4f3074c88a65af12332be91189df4
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-31 19:54:19 +0200

    navigation

--- views/order/step3_summary.php
+++ views/order/step3_summary.php
@@ -17,3 +17,23 @@ if (!is_array($addressInfo)) {
 $shippingType = (int)($data['postType'] ?? 1);
 $finalTotal = $totalProductsPrice + $shippingPrice - $totalDiscount;
 ?>
+<div class="checkout-modern-container order-page-wrapper">
+
+    <div class="checkout-grid-layout">
+        
+        <div class="checkout-left-column">
+            
+            <div class="checkout-back-nav">
+                <a href="<?= URL ?>Order/address" class="link-back-navigation"><i class="fa-solid fa-chevron-left" aria-hidden="true"></i> Retour à la livraison</a>
+            </div>
+
+            <nav class="checkout-stepper-modern-bar">
+                <ul class="stepper-steps-flex">
+                    <li class="completed">Connexion</li>
+                    <li class="completed">Livraison</li>
+                    <li class="active" aria-current="step">Résumé</li>
+                    <li>Paiement</li>
+                </ul>
+            </nav>
+
+           
