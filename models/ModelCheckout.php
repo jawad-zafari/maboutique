5bdb61841b1ae96f33050011da3e44b78bf95284
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-31 20:47:25 +0200

    récupérer les informations d'une commande de manière sécurisée

--- models/ModelCheckout.php
+++ models/ModelCheckout.php
@@ -7,6 +7,22 @@ class ModelCheckout extends Model
         parent::__construct();
     }
 
-   
+    // Récupère les informations d'une commande (Sécurisé contre IDOR)
+    public function getOrderInfo(int $orderId): array|false
+    {
+        self::sessionInit();
+        $userId = (int)self::sessionGet('userId');
+
+        if ($userId > 0) {
+            $sql = "SELECT * FROM orders WHERE id = ? AND user_id = ?";
+            $result = $this->doSelect($sql, [$orderId, $userId], 'fetch', PDO::FETCH_ASSOC);
+            
+            return $result ?: false;
+        }
+        
+        return false;
+    }
+
+    
 }
 ?>
