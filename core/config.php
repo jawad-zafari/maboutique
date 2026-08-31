a91c48ee958d52eb7daa4731070bafd8792f3888
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-31 21:04:54 +0200

    gestion sécurisée des paramètres depuis la base de données

--- core/config.php
+++ core/config.php
@@ -6,3 +6,10 @@ ini_set('session.cookie_httponly', 1);
 ini_set('session.use_only_cookies', 1);
 ini_set('session.cookie_samesite', 'Lax');
 
+// Chargement sécurisé des paramètres depuis la base de données
+try {
+    $options = Model::getoption();
+} catch (Exception $e) {
+    $options = [];
+}
+
