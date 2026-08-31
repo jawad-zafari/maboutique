7fc52a0597788dc052cb994954f0812ff804b4ea
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-31 15:00:50 +0200

    supprimer un article du panier

--- models/ModelCart.php
+++ models/ModelCart.php
@@ -13,6 +13,15 @@ class ModelCart extends Model
         return parent::getCart();
     }
 
-    
+    public function deleteCartItem(int $cartRowId): void
+    {
+        $cookie = parent::getCartCookie();
+
+        // vérifie que la ligne appartient bien au visiteur actuel
+        $sql = "DELETE FROM cart_items WHERE id = ? AND session_cookie = ?";
+        $this->doQuery($sql, [$cartRowId, $cookie]);
+    }
+
+   
 }
 ?>
