6ff1eec601cf09683b133da18e4f54f2dabf0115
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-31 15:08:33 +0200

    afficher le contenu du panier

--- controllers/Cart.php
+++ controllers/Cart.php
@@ -9,6 +9,21 @@ class Cart extends Controller
         Model::sessionInit();
     }
 
-    
+    // Afficher la page complète du panier
+    public function index(): void
+    {
+        $cartData = $this->model->getCartData();
+        
+        // Génération du jeton pour sécuriser les actions du panier
+        $data = [
+            'cartItems'     => $cartData[0] ?? [],
+            'priceTotalAll' => $cartData[1] ?? 0,
+            'csrf_token'    => $this->generateCsrfToken()
+        ];
+
+        $this->view('cart/cart', $data);
+    }
+
+   
 }
 ?>
