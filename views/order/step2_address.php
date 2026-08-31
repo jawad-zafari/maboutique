c9838ae7835093fe85acb70ddf03f3acd751ab47
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-31 17:03:48 +0200

    Ajout du jeton CSRF et du conteneur

--- views/order/step2_address.php
+++ views/order/step2_address.php
@@ -7,3 +7,7 @@ $totalDiscount = (float)($data['cartData'][2] ?? 0);
 $addresses = $data['addresses'] ?? [];
 $postTypes = $data['postType'] ?? [];
 ?>
+<!-- Jeton CSRF sécurisé avec la méthode $this->e() -->
+<div class="checkout-modern-container order-page-wrapper" id="step2AddressWrapper" data-csrf="<?= $this->e($data['csrf_token'] ?? '') ?>">
+
+    
