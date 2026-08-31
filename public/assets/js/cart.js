de7adf0e3671cc992661949fcbffed65ee896526
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-31 15:24:31 +0200

    les constant pour la gestion du panier

---
+++ public/assets/js/cart.js
@@ -0,0 +1,13 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const cartOverlay = document.getElementById('cartOverlay');
+    const cartSidebar = document.getElementById('cartSidebar');
+    const closeCartBtn = document.getElementById('closeCartBtn');
+    const headerCartBtns = document.querySelectorAll('.cart-btn'); 
+
+    const baseTag = document.querySelector('base');
+    const baseUrl = baseTag ? baseTag.getAttribute('href') : '/';
+
+   
+});
