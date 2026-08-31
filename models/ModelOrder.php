d66a43435b87438cb59b0052223e48b5c37afc20
jawad zafari <jawad.zafari@laplateforme.io>
2026-08-31 16:35:43 +0200

    récupérer une adresse utilisateur par ID

--- models/ModelOrder.php
+++ models/ModelOrder.php
@@ -15,6 +15,13 @@ class ModelOrder extends Model
         return $this->doSelect($sql, [$userId]);
     }
 
-   
+    public function getAddressById(int $addressId, int $userId): array
+    {
+        $sql = "SELECT * FROM user_addresses WHERE id = ? AND user_id = ?";
+        $result = $this->doSelect($sql, [$addressId, $userId], 'fetch', PDO::FETCH_ASSOC);
+        return $result ?: [];
+    }
+
+    
 }
 ?>
