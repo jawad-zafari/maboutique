16407b20a280f1ee35563897cb8f0e6e30cf88f4
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-01 21:34:35 +0200

    gestion de la connexion administrateur

---
+++ public/assets/js/admin_login.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const loginForm = document.getElementById('adminLoginForm');
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+    const errorContainer = document.getElementById('jsLoginErrorMessage');
+
+   
+});
