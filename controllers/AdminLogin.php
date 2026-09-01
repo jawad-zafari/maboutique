611a1ef6fbdb4ee9d88931705a1291ef82843eec
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-01 14:15:37 +0200

    méthode logout pour gérer la destruction de la session utilisateur

--- controllers/AdminLogin.php
+++ controllers/AdminLogin.php
@@ -59,6 +59,23 @@ class AdminLogin extends Controller
         }
     }
 
-   
+    public function logout()
+    {
+        Model::sessionInit();
+        
+        // Destruction complète des données de la session en cours
+        $_SESSION = array();
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]
+            );
+        }
+        session_destroy();
+
+        header('Location: ' . URL . 'AdminLogin/index');
+        exit;
+    }
 }
 ?>
