808bf103eeb30abe1b9cf2138a0ed954b72a35f1
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-01 16:24:22 +0200

    scripts nécessaires

---
+++ views/admin/footer.php
@@ -0,0 +1,9 @@
+</main> 
+</div> 
+
+    <script src="<?= URL ?>public/assets/js/admin.js" defer></script>
+    <script src="<?= URL ?>public/assets/js/admin_layout.js" defer></script>
+    
+    
+</body>
+</html>
