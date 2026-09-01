6597a64ae75bd44c6db0b7d8d33b887da417ee7d
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-01 21:46:23 +0200

    structure d'en-tête

--- views/admin/admin_product/add_product.php
+++ views/admin/admin_product/add_product.php
@@ -3,3 +3,17 @@ $productInfo = $data['productInfo'] ?? [];
 $isEdit = isset($productInfo['title']);
 $pId = (int)($productInfo['id'] ?? 0);
 ?>
+<div class="admin-container">
+    
+    <header class="admin-header">
+        <div class="admin-breadcrumb" aria-label="Fil d'Ariane">
+            <i class="fa-solid fa-box" aria-hidden="true"></i> <?= $isEdit ? 'Modifier le produit' : 'Créer un nouveau produit' ?>
+        </div>
+        <div class="admin-actions">
+            <a href="<?= URL ?>AdminProduct/index" class="btn-admin-back" aria-label="Retourner à la liste des produits">
+                <i class="fa-solid fa-arrow-left" aria-hidden="true"></i> Retour
+            </a>
+        </div>
+    </header>
+
+    
