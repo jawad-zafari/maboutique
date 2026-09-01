f53b2e7198a2b693d6b4de322bbfd8a80f0bd8f4
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-01 21:23:02 +0200

    suppression des images de la galerie

--- controllers/AdminProduct.php
+++ controllers/AdminProduct.php
@@ -94,6 +94,24 @@ class AdminProduct extends Controller
         exit;
     }
 
+    public function deleteGallery(int $productId): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            header('HTTP/1.1 405 Method Not Allowed');
+            exit('Méthode non autorisée');
+        }
+
+        $this->checkCsrfToken($_POST['csrf_token'] ?? '');
+        
+        $ids = $_POST['id'] ?? [];
+        if (!empty($ids) && is_array($ids)) {
+            $this->model->deleteGallery($ids);
+        }
+        
+        header('Location: ' . URL . 'AdminProduct/gallery/' . $productId . '?success=image_deleted');
+        exit;
+    }
+
    
 }
 ?>
