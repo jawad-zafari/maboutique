9bb412813b62636cd443d7d22aecbb08fff0cff8
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-01 21:05:35 +0200

    récupérer l'image d'un produit

--- models/ModelAdminProduct.php
+++ models/ModelAdminProduct.php
@@ -7,6 +7,19 @@ class ModelAdminProduct extends Model
         parent::__construct();
     }
 
+    public function findProductImage(int $id, int $size = 220): string 
+    {
+        $basePath = 'public/images/products/' . $id . '/product_' . $size;
+        $extensions = ['jpg', 'webp', 'png', 'jpeg'];
+        
+        foreach ($extensions as $ext) {
+            if (file_exists($basePath . '.' . $ext)) {
+                return URL . $basePath . '.' . $ext . '?v=' . time();
+            }
+        }
+        return '';
+    }
+
     
 }
 ?>
