a17b2fa1a3aece46925ba64cc7563dd8f28164bf
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-01 20:08:52 +0200

    ajouter la vue pour la gestion des valeurs par défaut des attributs

---
+++ views/admin/admin_category/attr_val.php
@@ -0,0 +1,16 @@
+<div class="admin-container">
+    
+    <header class="admin-header">
+        <div class="admin-breadcrumb" aria-label="Fil d'Ariane">
+            <i class="fa-solid fa-tags" aria-hidden="true"></i> Valeurs par défaut de l'attribut
+            <span class="separator">/</span>
+            <span class="active-breadcrumb-item"><?= $this->e($data['attrInfo']['title'] ?? '') ?></span>
+        </div>
+        <div class="admin-actions">
+            <a href="javascript:history.back()" class="btn-admin-back" aria-label="Retour à la page précédente">
+                <i class="fa-solid fa-arrow-left" aria-hidden="true"></i> Retour
+            </a>
+        </div>
+    </header>
+
+    
