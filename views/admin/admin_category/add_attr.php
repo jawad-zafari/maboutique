d7d3f3ddc4a8cd20337699814b3eb1792b4ec302
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-01 20:07:00 +0200

    gestion des attributs

--- views/admin/admin_category/add_attr.php
+++ views/admin/admin_category/add_attr.php
@@ -2,3 +2,23 @@
 $editInfo = $data['editInfo'] ?? [];
 $isEdit = !empty($editInfo['title']);
 ?>
+<div class="admin-container">
+    
+    <header class="admin-header">
+        <div class="admin-breadcrumb" aria-label="Fil d'Ariane">
+            <i class="fa-solid fa-pen-to-square" aria-hidden="true"></i> 
+            <?= !$isEdit ? 'Créer un nouvel attribut' : 'Modifier l\'attribut' ?>
+            <span class="separator">/</span>
+            <span class="sub-breadcrumb-info">
+                Catégorie : <?= $this->e($data['categoryInfo']['title'] ?? '') ?> 
+                <?= !empty($data['attrInfo']['id']) ? '| Parent : ' . $this->e($data['attrInfo']['title'] ?? '') : '' ?>
+            </span>
+        </div>
+        <div class="admin-actions">
+            <a href="<?= URL ?>AdminCategory/showAttributes/<?= (int)($data['categoryInfo']['id'] ?? 0) ?>/<?= (int)($data['parentId'] ?? 0) ?>" class="btn-admin-back" aria-label="Retour à la liste des attributs">
+                <i class="fa-solid fa-arrow-left" aria-hidden="true"></i> Retour
+            </a>
+        </div>
+    </header>
+
+   
