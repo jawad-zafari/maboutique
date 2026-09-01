3c513827445c88fab4000ab39d42f426aa217dc6
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-01 20:29:59 +0200

    sélection/désélectionner tout pour les cases à cocher

--- public/assets/js/admin_category.js
+++ public/assets/js/admin_category.js
@@ -88,6 +88,18 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-   
+    // 5. FONCTIONNALITÉ UX : SÉLECTIONNER / DÉSÉLECTIONNER TOUT
+    const selectAllCheckboxes = document.getElementById("selectAllCheckboxes");
+    
+    if (selectAllCheckboxes) {
+        selectAllCheckboxes.addEventListener("change", (event) => {
+            const isChecked = event.target.checked;
+            const rowCheckboxes = document.querySelectorAll(".row-checkbox");
+            
+            rowCheckboxes.forEach(checkbox => {
+                checkbox.checked = isChecked;
+            });
+        });
+    }
 
 });
