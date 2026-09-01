ed7db9600e6d2c9aa9a6220b0ab02cf9eff0d994
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-01 13:46:35 +0200

    interface administrateur

---
+++ views/admin/admin_dashboard/dashboard.php
@@ -0,0 +1,9 @@
+<div class="admin-container">
+    
+    <header class="admin-header">
+        <div class="admin-breadcrumb" aria-label="Fil d'Ariane">
+            <i class="fa-solid fa-gauge-high" aria-hidden="true"></i> Tableau de Bord
+        </div>
+    </header>
+
+   
