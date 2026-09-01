022844ad6b6f0454ee502c11bed5f6c5334c36e1
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-01 13:53:14 +0200

    Ajout du script pour le tableau de bord administrateur

---
+++ public/assets/js/admin_dashboard.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const chartContainer = document.getElementById('salesChartContainer');
+    
+    if (chartContainer) {
+        // Récupération sécurisée des données depuis les attributs HTML
+        const rawKeys = chartContainer.getAttribute('data-keys');
+        const rawValues = chartContainer.getAttribute('data-values');
+        
+        let categories = [];
+        let seriesData = [];
+
+       
+    }
+});
