e832c8d4d93c33d4783e47ebe581d9fbe95b4c6c
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-01 13:33:20 +0200

    gérer la protection contre les tentatives de connexion par force brute

--- models/ModelAdminLogin.php
+++ models/ModelAdminLogin.php
@@ -7,6 +7,26 @@ class ModelAdminLogin extends Model
         parent::__construct();
     }
 
+    public function checkUser($form)
+    {
+        Model::sessionInit();
+
+        // Protection contre la force brute (Max 3 tentatives, blocage 5 minutes)
+        $maxAttempts = 3;
+        $lockoutTime = 300; 
+
+        if (isset($_SESSION['login_attempts']) && $_SESSION['login_attempts'] >= $maxAttempts) {
+            if (time() - $_SESSION['last_attempt_time'] < $lockoutTime) {
+                return 'locked';
+            } else {
+                // Réinitialisation après expiration du temps de blocage
+                $_SESSION['login_attempts'] = 0;
+            }
+        }
+
+       
+    }
+
    
 }
 ?>
