7c7c12a7957f2310c64f2b7b4f57112fb2bb46b9
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-01 22:22:33 +0200

    ajouter header

--- views/admin/admin_product/add_review.php
+++ views/admin/admin_product/add_review.php
@@ -5,3 +5,22 @@ $isEdit = isset($reviewInfo['title']);
 $pId = (int)($productInfo['id'] ?? 0);
 $rId = (int)($reviewInfo['id'] ?? 0);
 ?>
+<div class="admin-container">
+    
+    <header class="admin-header">
+        <div class="admin-breadcrumb" aria-label="Fil d'Ariane">
+            <i class="fa-solid fa-pen-nib" aria-hidden="true"></i> 
+            <?= $isEdit ? 'Modifier la critique' : 'Ajouter une critique' ?>
+            <span class="separator">/</span>
+            <span class="sub-breadcrumb-info"><?= $this->e($productInfo['title'] ?? '') ?></span>
+        </div>
+        <div class="admin-actions">
+            <a href="#" class="btn-admin-back js-back-button" aria-label="Retour">
+                <i class="fa-solid fa-arrow-left" aria-hidden="true"></i> Retour
+            </a>
+        </div>
+    </header>
+
+   
+</div>
+<script src="<?= URL ?>public/assets/js/admin_product.js" defer></script>
