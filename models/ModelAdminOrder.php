297f22d0760becdc29c2ae344404dda5416a4a40
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-01 23:13:11 +0200

    récupérer les commandes avec leur statut

--- models/ModelAdminOrder.php
+++ models/ModelAdminOrder.php
@@ -7,6 +7,14 @@ class ModelAdminOrder extends Model
         parent::__construct();
     }
 
+    public function getOrders(): array
+    {
+        $sql = "SELECT o.*, os.title as statusTitle 
+                FROM orders o 
+                LEFT JOIN order_statuses os ON o.status_id = os.id 
+                ORDER BY o.id DESC";
+        $result = $this->doSelect($sql);
+        return is_array($result) ? $result : [];
+    }
+
     
-}
-?>
