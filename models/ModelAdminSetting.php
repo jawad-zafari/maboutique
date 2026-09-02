fa8722106b07845ddf104803d51ef0cc626b382e
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-02 09:36:20 +0200

    récupérer les paramètres de configuration

--- models/ModelAdminSetting.php
+++ models/ModelAdminSetting.php
@@ -6,8 +6,23 @@ class ModelAdminSetting extends Model
     {
         parent::__construct();
     }
-    
 
+    public function getSettings(): array
+    {
+        $sql = "SELECT * FROM settings";
+        $result = $this->doSelect($sql);
+        
+        $settings = [];
+        if (is_array($result)) {
+            foreach ($result as $row) {
+                $settings[$row['setting_key']] = $row['setting_value'];
+            }
+        }
+        
+        return $settings;
+    }
+
+    
     
 }
 ?>
