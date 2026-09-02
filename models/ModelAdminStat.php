05a90f3dc250047a8c3211421f8a5e8bdf2cd6b6
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-02 13:12:08 +0200

    ajout de la gestion des commandes entre deux dates

--- models/ModelAdminStat.php
+++ models/ModelAdminStat.php
@@ -19,5 +19,16 @@ class ModelAdminStat extends Model
             ];
         }
 
-    
+        // Ajout des heures pour couvrir la journée complète
+        $startDateTime = $startDate . ' 00:00:00';
+        $endDateTime = $endDate . ' 23:59:59';
+
+        // Requête SQL sécurisée pour récupérer les commandes entre les deux dates
+        $sql = "SELECT * FROM orders WHERE created_date BETWEEN ? AND ? ORDER BY created_date DESC";
+        $result = $this->doSelect($sql, [$startDateTime, $endDateTime]);
+        
+        $ordersPaid = 0;
+
+        
+}
 ?>
