6f038131ded7813170356ac4ec5ccd88870cd301
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-02 11:07:41 +0200

    Récupérer toutes les questions

--- models/ModelAdminQuestion.php
+++ models/ModelAdminQuestion.php
@@ -7,6 +7,21 @@ class ModelAdminQuestion extends Model
         parent::__construct();
     }
 
+    public function getQuestions(): array
+    {
+        $sql = "SELECT * FROM questions WHERE parent_id = 0 ORDER BY id DESC";
+        $questions = $this->doSelect($sql);
+
+        if (is_array($questions)) {
+            foreach ($questions as $key => $q) {
+                $sqlAnswer = "SELECT * FROM questions WHERE parent_id = ?";
+                $answer = $this->doSelect($sqlAnswer, [(int)$q['id']], 'fetch');
+                $questions[$key]['admin_answer'] = $answer ? $answer['content'] : '';
+            }
+        }
+        return is_array($questions) ? $questions : [];
+    }
+
    
 }
 ?>
