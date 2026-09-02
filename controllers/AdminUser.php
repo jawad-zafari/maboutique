6b3fbab29a9d6017fcf41a0180b6251f0239c19d
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-02 08:57:00 +0200

    gérer le changement de niveau des utilisateurs

--- controllers/AdminUser.php
+++ controllers/AdminUser.php
@@ -27,6 +27,24 @@ class AdminUser extends Controller
         $this->view('admin/admin_user/users', $data);
     }
 
+    public function changeLevel1(): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            header('HTTP/1.1 405 Method Not Allowed');
+            exit('Méthode non autorisée');
+        }
+
+        $this->checkCsrfToken($_POST['csrf_token'] ?? '');
+            
+        $ids = $_POST['id'] ?? [];
+        if (!empty($ids) && is_array($ids)) {
+            $this->model->changeLevel1($ids);
+        }
+        
+        header('Location: ' . URL . 'AdminUser/index');
+        exit;
+    }
+
     
 }
 ?>
