ac909cd6de133af4395ef1491066598ad2a41067
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-02 11:59:05 +0200

    ajouter la fonctionnalité de suppression des sliders

--- controllers/AdminSlider.php
+++ controllers/AdminSlider.php
@@ -102,6 +102,18 @@ class AdminSlider extends Controller
         exit;
     }
 
-    
+    public function delete(): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            header('HTTP/1.1 405 Method Not Allowed');
+            exit;
+        }
+
+        $this->checkCsrfToken($_POST['csrf_token'] ?? '');
+            
+        $this->model->delete($_POST);
+        header('Location: ' . URL . 'AdminSlider/index?success=delete');
+        exit;
+    }
 }
 ?>
