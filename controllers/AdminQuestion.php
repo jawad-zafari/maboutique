d33af140389dd7425f0bd20f56c54fb40d7090a5
jawad zafari <jawad.zafari@laplateforme.io>
2026-09-02 11:04:24 +0200

    gestion des questions administratives

---
+++ controllers/AdminQuestion.php
@@ -0,0 +1,20 @@
+<?php
+
+class AdminQuestion extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        
+        // Initialisation de la session pour vérifier le niveau d'accès
+        Model::sessionInit();
+        $level = (int) Model::getUserLevel();
+        if ($level !== 1) {
+            header('Location: ' . URL . 'AdminLogin/index');
+            exit;
+        }
+    }
+
+   
+}
+?>
